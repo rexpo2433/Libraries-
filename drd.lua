0bd96e802a4f94a6cb01762451c636794d242c64
rexpo2433 <rexpo2433@gmail.com>
2014-03-26 12:19:18 +0200

    Create drd.lua

---
+++ drd.lua
@@ -0,0 +1,59 @@
+--HeroName = {
+--	spell = ability number,
+--	ran = {castRange} }, 				cast range for every lvl abiliy
+
+rangelist = {
+
+npc_dota_hero_slark = {
+ spell = 2,
+ ran = {700,700,700,700}},
+ 
+npc_dota_hero_lone_druid = {
+ spell = 1,
+ ran = {1100,1100,1100,1100}},
+ 
+npc_dota_hero_tinker = {
+ spell = 2,
+ ran = {2500,2500,2500,2500}},
+ 
+npc_dota_hero_axe = {
+ spell = 1,
+ ran = {300,300,300,300}},
+ 
+npc_dota_hero_queenofpain = {
+ spell = 2,
+ ran = {700,850,1000,1150}},
+   
+npc_dota_hero_treant = {
+ spell = 4,
+ ran = {625,625,625}},
+ 
+npc_dota_hero_antimage = {
+ spell = 2,
+ ran = {1000,1000,1150,1150}},
+ 
+npc_dota_hero_gyrocopter = {
+ spell = 3,
+ ran = {1000,1000,1000,1000}},
+ 
+npc_dota_hero_slardar = {
+ spell = 2,
+ ran = {350,350,350,350}},
+ 
+npc_dota_hero_necrolyte = {
+ spell = 1,
+ ran = {475,475,475,475}},
+ 
+npc_dota_hero_mirana = {
+ spell = 1,
+ ran = {625,625,625,625}}, 
+
+npc_dota_hero_ursa = {
+ spell = 1,
+ ran = {385,385,385,385}},
+ 
+ npc_dota_hero_phoenix = {
+ spell = 1,
+ ran = {1400,1400,1400,1400}},
+ 
+}
